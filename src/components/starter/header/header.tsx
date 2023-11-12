382eb9d9b3674204e8c4ac8407388b9be56ecd30
Kohpai <whcwhc78@gmail.com>
2023-11-12 21:19:52 +0700

    :recycle: move nav bar to header

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,43 +1,32 @@
 import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
 import styles from "./header.module.css";
 
 export default component$(() => {
   return (
     <header class={styles.header}>
-      <div class={["container", styles.wrapper]}>
-        <div class={styles.logo}>
-          <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
+      <nav>
+        <div class="container mx-auto px-6 py-2 flex justify-between items-center">
+          <a class="font-bold text-2xl lg:text-4xl" href="#">
+            SHMW
           </a>
+          <div class="block lg:hidden">
+            <button
+              class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:border-teal-500 appearance-none focus:outline-none">
+              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <div class="hidden lg:block">
+            <ul class="inline-flex">
+              <li><a class="px-4 font-bold" href="/">Home</a></li>
+              <li><a class="px-4 hover:text-gray-800" href="#">About</a></li>
+              <li><a class="px-4 hover:text-gray-800" href="#">Contact</a></li>
+            </ul>
+          </div>
         </div>
-        <ul>
-          <li>
-            <a
-              href="https://qwik.builder.io/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
-            </a>
-          </li>
-        </ul>
-      </div>
+      </nav>
     </header>
   );
 });
